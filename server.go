09eb5c91317011dc82eba8045f7e46835e9c46fc
kaichung92 <kai.chung92@hotmail.com>
2022-06-12 00:15:27 +0800

    socket v1

--- server.go
+++ server.go
@@ -4,45 +4,94 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/hello" {
-        http.Error(w, "404 not found, from handler.", http.StatusNotFound)
-        return
-    }
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found, from handler.", http.StatusNotFound)
+		return
+	}
 
-    if r.Method != "GET" {
-        http.Error(w, "Method is not supported, from handler.", http.StatusNotFound)
-        return
-    }
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported, from handler.", http.StatusNotFound)
+		return
+	}
 
-    fmt.Fprintf(w, "Hello!")
+	fmt.Fprintf(w, "Hello!")
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		// read in a message
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// print out that message for clarity
+		log.Println("sending from here")
+		fmt.Println("text" + string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// upgrade this connection to a WebSocket
+	// connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client Connected")
+	err = ws.WriteMessage(1, []byte("Hi Client!"))
+	if err != nil {
+		log.Println(err)
+	}
+	// listen indefinitely for new messages coming
+	// through on our WebSocket connection
+	reader(ws)
+
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-    if err := r.ParseForm(); err != nil {
-        fmt.Fprintf(w, "ParseForm() err: %v", err)
-        return
-    }
-    fmt.Fprintf(w, "POST from post handler " + r.Method)
-    name := r.FormValue("name")
-    address := r.FormValue("address")
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST from post handler "+r.Method)
+	name := r.FormValue("name")
+	address := r.FormValue("address")
 
-    fmt.Fprintf(w, "Name = %s\n", name)
-    fmt.Fprintf(w, "Address = %s\n", address)
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
 func main() {
-    fileServer := http.FileServer(http.Dir("./static")) 
-    
+	fileServer := http.FileServer(http.Dir("./static"))
+
 	http.HandleFunc("/form", formHandler)
-    http.HandleFunc("/hello", helloHandler) 
-    http.Handle("/", fileServer) 
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/ws", wsEndpoint)
 
+	http.Handle("/", fileServer)
 
-    fmt.Printf("Starting server at port 8080\n")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatal(err)
-    }
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
