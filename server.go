51c1b1bf837c8f44860dc1c9091b885e598b7027
Noel Lim <noellimx@gmail.com>
2022-06-12 22:05:44 +0800

    local https instead of http

--- server.go
+++ server.go
@@ -7,42 +7,45 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/hello" {
-        http.Error(w, "404 not found, from handler.", http.StatusNotFound)
-        return
-    }
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found, from handler.", http.StatusNotFound)
+		return
+	}
 
-    if r.Method != "GET" {
-        http.Error(w, "Method is not supported, from handler.", http.StatusNotFound)
-        return
-    }
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported, from handler.", http.StatusNotFound)
+		return
+	}
 
-    fmt.Fprintf(w, "Hello!")
+	fmt.Fprintf(w, "Hello!")
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-    if err := r.ParseForm(); err != nil {
-        fmt.Fprintf(w, "ParseForm() err: %v", err)
-        return
-    }
-    fmt.Fprintf(w, "POST from post handler " + r.Method)
-    name := r.FormValue("name")
-    address := r.FormValue("address")
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST from post handler "+r.Method)
+	name := r.FormValue("name")
+	address := r.FormValue("address")
 
-    fmt.Fprintf(w, "Name = %s\n", name)
-    fmt.Fprintf(w, "Address = %s\n", address)
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
 func main() {
-    fileServer := http.FileServer(http.Dir("./static")) 
-    
+	fileServer := http.FileServer(http.Dir("./static"))
+
 	http.HandleFunc("/form", formHandler)
-    http.HandleFunc("/hello", helloHandler) 
-    http.Handle("/", fileServer) 
+	http.HandleFunc("/hello", helloHandler)
+	http.Handle("/", fileServer)
 
+	fmt.Printf("Starting server at port 8080\n")
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	//     log.Fatal(err)
+	// }
+	if err := http.ListenAndServeTLS(":10443", "/Users/noellim/customkeystore/server.cert", "/Users/noellim/customkeystore/server.key", nil); err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Printf("Starting server at port 8080\n")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatal(err)
-    }
 }
