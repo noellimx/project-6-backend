5905a6ad0217b46a32191d2b1ee466131f3081f6
kaichung92 <kai.chung92@hotmail.com>
2022-06-15 03:38:02 +0800

    ticker

--- routes/main_test.go
+++ routes/main_test.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -20,6 +21,29 @@ func TestTickerListHandler(t *testing.T) {
 	if wantResponseType != gotResponseType {
 		t.Error("Wrong Response Type")
 	}
+
+	type TickerListResponseBody struct {
+		Tickers []string
+	}
+
+	var bodyBytes []byte
+	tlRB := &TickerListResponseBody{}
+
+	res.Body.Read(bodyBytes)
+
+	json.Unmarshal(bodyBytes, tlRB)
+
+	notWantedTicketCount := 0
+	gotTickerCount := len(tlRB.Tickers)
+
+	if notWantedTicketCount == gotTickerCount {
+		t.Errorf("ticker Count Empty")
+	}
+
+	// {
+	//     "tickers" : ["AAPL", "MOON"]
+	// }
+
 	// data, err := ioutil.ReadAll(res.Body)
 	// if err != nil {
 	//     t.Errorf("expected error to be nil got %v", err)
--- routes/ticker.go
+++ routes/ticker.go
@@ -3,5 +3,5 @@ package routes
 import "net/http"
 
 func GetAllTickers(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
 }
