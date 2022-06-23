f2c12567530bd0944d80a519ebea1e3832925fc3
Noel Lim <noellimx@gmail.com>
2022-06-23 23:10:16 +0800

    deploy script scp from pem path

--- dev-hooks/deployment-hooks/trigger-deploy.sh
+++ dev-hooks/deployment-hooks/trigger-deploy.sh
@@ -24,7 +24,9 @@ deploy_script_path=$4
 echo "Copying deployment script to ec2 instance ${ec2}"
 
 # ping -n 1 -W 3 ${ec2_ip}
-echo "pinging 54.255.120.79"
+echo "pinging 54.255.120.79 fruitt.art"
+sudo ping -n 1 -W 3 fruitt.art
+sudo ping -n 1 -W 3 54.255.120.79
 exit 1;
 
 # ssh -tt -i "${pem_full_path}" "${ec2}" "df -H"
