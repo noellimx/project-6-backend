1be6dbc4f54d780cc4f1a49687295696f083accc
Noel Lim <noellimx@gmail.com>
2022-06-23 22:10:19 +0800

    deploy script path

--- dev-hooks/deployment-hooks/trigger-deploy.sh
+++ dev-hooks/deployment-hooks/trigger-deploy.sh
@@ -23,17 +23,14 @@ commit_hash=$3
 deploy_script_path=$4
 cat $pem_full_path
 
-echo "Supply configuration file to ${ec2}"
 
-ssh -tt -i "${pem_full_path}" "${ec2}" "sudo rm -rf ${ec2_config_dir}"
-scp -rt -i "${pem_full_path}" ${ec2_config_dir} "${ec2}":"${ec2_config_dir}"
+
+echo "Copying deployment script to ec2 instance"
+scp -tt -i ${pem_full_path} ${deploy_script_path} "${ec2}":./ 
 
 echo $deploy_script_path
 exit 1;
 
-
-echo "Copying deployment script to ec2 instance"
-scp -i ${pem_full_path} ./some.sh "${ec2}":./ 
 
 echo "Execute script on server environment"
 ssh -t -i "${pem_full_path}" "${ec2}" "chmod +x ${deploy_script_path} && ${deploy_script_path} ${commit_hash}"
